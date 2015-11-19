7acc595067e223fdbee9e40e5904cc0bf143f0c3
david siah <dsiah@villanova.edu>
2015-11-19 00:26:54 -0500

    private file excludes specifics -- could be command line supplied or file (TODECIDE)

---
+++ request_handler.py
@@ -0,0 +1,10 @@
+import requests
+import private
+
+print private.url
+print private.cookies
+print private.headers
+print private.data
+
+#course_listing_request = requests.post(url, headers=headers, cookies=cookies, data=data)
+
