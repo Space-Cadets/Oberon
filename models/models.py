899a5be2abb141789ec074af1110df15123b498f
david siah <dsiah@villanova.edu>
2015-11-20 10:08:15 -0500

    more model work

--- models/models.py
+++ models/models.py
@@ -1,41 +1,79 @@
-#from flask import flask
-from sqlalchemy import *
-from sqlalchemy.engine.url import URL
 import os, sys
+from sqlalchemy.engine.url      import URL
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm             import relationship, backref
+from sqlalchemy					import ForeignKey, Column
+from sqlalchemy                 import create_engine, Integer, String, DateTime
 
 # Relative path support -- great snippet
 lib_path = os.path.abspath(os.path.join('..'))
 sys.path.append(lib_path)
+# You need to add db credentials
+import private 
 
-import private
+DeclarativeBase = declarative_base()
 
-print private.db
+class Student(DeclarativeBase):
+	# SQL Alchemy Models structured like so
+	__tablename__ = "students"
+
+	student_id    = Column(Integer, primary_key=True)
+	email         = Column('email', String)
+	first_name    = Column('first_name', String)
+	last_name     = Column('last_name', String)
+	password      = Column('phash', String)
+	date_created  = Column('date_created', DateTime)
+
+	reviews = relationship('reviews', order_by='review.id', backref='student')
+
+
+class Review(DeclarativeBase):
+	__tablename__ = "reviews"
+
+	review_id     = Column(Integer, primary_key=True)
+	user_id       = Column(Integer, ForeignKey('user.id'))
+
+	student       = relationship('student', backref=backref('addresses', order_by=review_id))
+
+class Location(DeclarativeBase):
+	__tablename__ = "locations"
+
+	location_id   = Column(Integer, primary_key=True)
+	location_name = Column('location_name', String)
+
+class Instructor(DeclarativeBase):
+	__tablename__ = "instructors"
+
+	instructor_id = Column(Integer, primary_key=True)
+	title         = Column('title', String)
+	department    = Column('department', String) # Needs to be a relationship 
 
 """
-class Student(db.Model):
-    pass
+To Implement in Schema
 
-class Course(db.Model):
-    pass
+class Section(DeclarativeBase):
+	__tablename__ = "sections"
 
-class Section(db.Model):
-    pass
+class Attribute(DeclarativeBase):
+	__tablename__ = "attributes"
 
-class Restriction(db.Model):
-    pass
+class Semester(DeclarativeBase):
+	__tablename__ = "semester"
 
-class Attribute(db.Model):
-    pass
+class Course(DeclarativeBase):
+	__tablename__ = "courses"
 
-class Instructor(db.Model):
-    pass
-
-class Review(db.Model):
-    pass
-
-class Semester(db.Model):
-    pass
-
-class Location(db.Model):
-    pass
+class Restriction(DeclarativeBase):
+	__tablename__ = "restrictions"
+	
 """
+
+def db_connect():
+	"""
+	Performs database connection using database settings from settings.py.
+	Returns sqlalchemy engine instance
+	"""
+	return create_engine(URL(**private.db))
+
+def create_tables(engine):
+	DeclarativeBase.metadata.create_all(engine)
