7bc94a16a821ed1e0e8fde5a727606f25f302388
david siah <dsiah@villanova.edu>
2015-11-20 11:21:04 -0500

    more comments and revisions to the schema

--- models/models.py
+++ models/models.py
@@ -5,7 +5,8 @@ from sqlalchemy.orm             import relationship, backref
 from sqlalchemy					import ForeignKey, Column
 from sqlalchemy                 import create_engine, Integer, String, DateTime
 
-# Relative path support -- great snippet
+
+# Relative path support -> great snippet
 lib_path = os.path.abspath(os.path.join('..'))
 sys.path.append(lib_path)
 # You need to add db credentials
@@ -17,51 +18,79 @@ class Student(DeclarativeBase):
 	# SQL Alchemy Models structured like so
 	__tablename__ = "students"
 
-	student_id    = Column(Integer, primary_key=True)
-	email         = Column('email', String)
-	first_name    = Column('first_name', String)
-	last_name     = Column('last_name', String)
-	password      = Column('phash', String)
-	date_created  = Column('date_created', DateTime)
+	student_id    = Column(Integer, primary_key=True) # Each table needs one of these
+	email         = Column(String)
+	first_name    = Column(String)
+	last_name     = Column(String)
+	password      = Column(String)
+	date_created  = Column(DateTime)
 
-	reviews = relationship('reviews', order_by='review.id', backref='student')
+	# This is how to establish relationship in the ORM
+	reviews = relationship('Review', order_by='review.id', backref='student')
 
 
 class Review(DeclarativeBase):
 	__tablename__ = "reviews"
 
-	review_id     = Column(Integer, primary_key=True)
-	user_id       = Column(Integer, ForeignKey('user.id'))
+	review_id     = Column(Integer, primary_key=True) 
+	student_id    = Column(Integer, ForeignKey('student.id'))
+	course_id     = Column(Integer, ForeignKey('course.id'))
+	section_id    = Column(Integer, ForeignKey('section.id'))
+	class_rating  = Column(Integer)
+	inst_rating   = Column(Integer)                    
+	instructor    = Column(String)                    # Maybe Int Foreign key
+	course_name   = Column(String)
+	review_body   = Column(String,  nullable=True)    # Nullable flag means not required
 
-	student       = relationship('student', backref=backref('addresses', order_by=review_id))
+	# Needs relationship for tags, instructor, and course as well
+	student       = relationship('Student', order_by='student.id', backref='review')
+	course        = relationship('Course',  order_by='course.id',  backref='review')
 
 class Location(DeclarativeBase):
 	__tablename__ = "locations"
 
 	location_id   = Column(Integer, primary_key=True)
-	location_name = Column('location_name', String)
+	location_name = Column(String)
 
 class Instructor(DeclarativeBase):
 	__tablename__ = "instructors"
 
 	instructor_id = Column(Integer, primary_key=True)
-	title         = Column('title', String)
-	department    = Column('department', String) # Needs to be a relationship 
-
-"""
-To Implement in Schema
+	title         = Column(String)
+	department    = Column(String) # Needs to be a relationship 
+	course_list   = Column(String) # (TODO) create join table and fix this relationship
 
 class Section(DeclarativeBase):
 	__tablename__ = "sections"
 
+	section_id    = Column(Integer, primary_key=True)
+	days          = Column(String,  nullable=True)
+	enrollment    = Column(String,  nullable=True) 
+
+
 class Attribute(DeclarativeBase):
 	__tablename__ = "attributes"
 
-class Semester(DeclarativeBase):
-	__tablename__ = "semester"
+	attribute_id  = Column(Integer, primary_key=True) 
+
 
 class Course(DeclarativeBase):
 	__tablename__ = "courses"
+
+	course_id     = Column(Integer, primary_key=True) 
+
+class Restriction(DeclarativeBase):
+	__tablename__ = "restrictions"
+
+	restiction_id = Column(Integer, primary_key=True)
+	text          = Column(String)	
+
+"""
+To Implement in Schema
+
+Veto making semester -- just do key or separate DBs with the same schema
+class Semester(DeclarativeBase):
+	__tablename__ = "semester"
 
 class Restriction(DeclarativeBase):
 	__tablename__ = "restrictions"
